b6f2a1c1f8da39216d53cdddf1b07b42383ba702
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2023-07-31 19:54:46 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,31 @@
+
+#variables
+vaso = "un papel lleno de codigo"
+numero = 50
+fin = 45
+bool = str(fin)
+cadena = bool
+
+#print de las variables
+print(cadena)
+print(complex(fin))
+
+#concatenacion por variables y por strings
+
+print(cadena, bool, fin)
+print("cuando tenemos un vaso en vscode es" , vaso)
+
+#funciones del sistema
+
+print(len(vaso)) # contar caracteres de la variable
+
+firstname = input("what's your name ") # input llama a la variable para ponerle un nombre cuando estemos en la terminal
+myage = input("how old are you?")
+
+print ("okey soo your name is ", firstname, "and you are " , myage)
+
+#declaracion de variables en una sola linea de codigo
+
+fname, lname, age, = "sebastian", "lopez", 16
+print("hey what's up my first name is" ,fname, "and my last name is" ,lname, "im", age, "years old")
+
